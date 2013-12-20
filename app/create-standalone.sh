5d536f74c3ffffd8630ca13b3a732266a21f6714
Sebastian Muelhausen <sebastian.muelhausen@br-automation.com>
2013-12-20 13:51:17 +0100

    [TASK] Add start of flash download in standalone script
    Change-Id: Ic24750be0fcc9e0962abf9e4b6619761f132a955
    Reviewed-on: http://brateggevoat1/gerrit/1712
    Tested-by: Jenkins <jenkins@brateggevoat1.br-automation.co.at>
    Reviewed-by: Joerg Zelenka <joerg.zelenka@br-automation.com>

--- app/create-standalone.sh
+++ app/create-standalone.sh
@@ -12,6 +12,7 @@ source create-this-app.settings
 STANDALONE_DIR="./standalone"
 TOOL_DIR="$FM_DIR/tools"
 
+DOWNLOAD=
 
 while [ $# -gt 0 ]
 do
@@ -21,9 +22,14 @@ do
           STANDALONE_DIR=$1
           echo "INFO: Overwritten STANDALONE_DIR to $STANDALONE_DIR"
           ;;
+      --start-download)
+          DOWNLOAD=1
+          echo "INFO: Flash-download will be started at the end"
+          ;;
       --help)
           echo "Usage: ${0} [OPTION]"
           echo
+          echo "  --start-download      device will be flashed automatically"
           echo "  --standalonedir PATH  path to stand-alone folder"
           echo
           exit 1
@@ -33,16 +39,18 @@ do
 done
 
 
-
+#Makefile check
 if [ ! -f ./Makefile ]; then
     echo "Missing Makefile!"
     echo "Please run create-this-app first"
     exit 1
 fi
 
+#Create folder
 echo "Create standalone-folder $STANDALONE_DIR"
 mkdir "$STANDALONE_DIR"
 
+#Download files
 echo "Copy sof-file $STANDALONE_DIR/standalone.sof"
 SOPC_SOF=`ls $SOPC_DIR*.sof`
 cp "$SOPC_SOF" "$STANDALONE_DIR/standalone.sof" || {
@@ -63,17 +71,28 @@ cp "$TOOL_DIR/altera-nios2/flash-device.sh" "$STANDALONE_DIR/flash-device.sh" ||
     exit 1
 }
 
+#store SysID
 echo "Create SysID-file $STANDALONE_DIR/SysID.data"
 make print-sysid > "$STANDALONE_DIR/SysID.data" || {
     echo "Create SysID-file failed"
     exit 1
 }
 
+#store base-address
 echo "Create base-address-file $STANDALONE_DIR/base.data"
 make print-base-address > "$STANDALONE_DIR/base.data" || {
     echo "Create base-address-file failed"
     exit 1
 }
+
+#Download flash
+if [ "$DOWNLOAD" ]; then
+    chmod +x $STANDALONE_DIR/flash-device.sh
+    $STANDALONE_DIR/flash-device.sh --sourcedir $STANDALONE_DIR || {
+        echo "Download-flash failed"
+        exit 1
+    }
+fi
 
 
 exit 0
